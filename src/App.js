58abb143c9dc1ac7e7c90a9df66c8edd3300adb4
haroonbajwa <haroonbajwa99@gmail.com>
2021-09-05 00:34:10 +0500

    search video feature

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
 import React from 'react'
+import { Grid } from '@material-ui/core'
+
+import { SearchBar, VideoList, VideoDetail } from './components'
+import youtube from './api/youtube'
 
 const App = () => {
+
+    const handleSubmit = async (searchTerm) => {
+        try {
+            const response = await youtube.get('search', {
+                params: {
+                    part: 'snippet',
+                    maxResults: 5,
+                    key: 'AIzaSyC5lsKNcy9PkCazwwmjYsds64brfTOKT4E',
+                    q: searchTerm,
+                }
+            });
+
+            console.log(response.data.items);
+
+        } catch(error) {
+            console.log(error);
+        }
+    }
+
     return (
-        <div>
-            <h1>Youtube Clone App</h1>
-        </div>
+        <Grid justifyContent="center" container spacing={10}>
+            <Grid item xs={12}>
+                <Grid container spacing={10}>
+                    <Grid item xs={12}>
+                        <SearchBar onFormSubmit={handleSubmit} />
+                    </Grid>
+                    <Grid item xs={8}>
+                        <VideoDetail />
+                    </Grid>
+                    <Grid item xs={4}>
+                        <VideoList />
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Grid>
     )
 }
 
---
+++ src/components/VideoDetail.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const VideoDetail = () => {
+    return (
+        <div>
+            Video detail component
+        </div>
+    )
+}
+
+export default VideoDetail
